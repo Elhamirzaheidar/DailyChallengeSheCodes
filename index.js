5f75aa0e9e8134063457e5e97b9a616227d4c5bb
Elhamirzaheidar <emirzaheidar@gmail.com>
2024-04-22 09:14:27 -0700

    EventChallenge

--- index.js
+++ index.js
@@ -48,29 +48,47 @@
 // console.log(now.getMonth());
 // console.log(now.getTime());
 // console.log(now.getFullYear());
-let Time = new Date();
-let Dayes = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-let Month = [
-  "Jan",
-  "Feb",
-  "March",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let Months = Month[Time.getMonth()];
-let Days = Dayes[Time.getDay()];
-console.log(Time);
-console.log(Time.getMilliseconds());
-console.log(Days);
-console.log(Time.getFullYear());
-console.log(Months);
-console.log(
-  `Today is ${Days},${Months}${Time.getMonth()},${Time.getFullYear()}`
-);
+// let Time = new Date();
+// let Dayes = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+// let Month = [
+//   "Jan",
+//   "Feb",
+//   "March",
+//   "Apr",
+//   "May",
+//   "Jun",
+//   "Jul",
+//   "Aug",
+//   "Sep",
+//   "Oct",
+//   "Nov",
+//   "Dec",
+// ];
+// let Months = Month[Time.getMonth()];
+// let Days = Dayes[Time.getDay()];
+// console.log(Time);
+// console.log(Time.getMilliseconds());
+// console.log(Days);
+// console.log(Time.getFullYear());
+// console.log(Months);
+// console.log(
+//   `Today is ${Days},${Months}${Time.getMonth()},${Time.getFullYear()}`
+// );
+
+
+
+// Challenge 1
+// Add click eventListener to the button. Alert `It is 18 degrees`
+
+// Challenge 2
+// When the button is clicked, replace button label by ‘18 degrees’
+
+
+
+let button = document.querySelector("button");
+button.addEventListener("click", showTemperature);
+function showTemperature(event) {
+  event.preventDefault();
+  alert("Temperature is 18 degrees");
+  button.innerHTML = "18 degrees";
+}
